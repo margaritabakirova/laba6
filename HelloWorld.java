62a6e45ce9f8928c712771e6c4bbe1728d343b52
student <student@GAMMA11.ASU.local>
2014-01-18 12:14:02 +0500

    ывпиыук

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,7 +15,7 @@ g.fillRect(0,0,d.width,d.height);
 g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("125411"
-		+ "<frbhjdf", d.width, d.height, g);
+		+ "Бакирова", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
 
